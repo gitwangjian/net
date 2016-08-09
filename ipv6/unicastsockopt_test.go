da49dfda9079b9a1fb40da775788c04d7ec4365e
gitwangjian <wangjianimustu@163.com>
2016-08-09 18:06:33 +0800

    Update unicastsockopt_test.go
    z

--- ipv6/unicastsockopt_test.go
+++ ipv6/unicastsockopt_test.go
@@ -9,9 +9,9 @@ import (
 	"runtime"
 	"testing"
 
-	"golang.org/x/net/internal/iana"
-	"golang.org/x/net/internal/nettest"
-	"golang.org/x/net/ipv6"
+	"gitwangjian/net/internal/iana"
+	"gitwangjian/net/internal/nettest"
+	"gitwangjian/net/ipv6"
 )
 
 func TestConnUnicastSocketOptions(t *testing.T) {
