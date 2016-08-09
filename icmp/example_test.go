2c9ec9c831e86d75107b8408237e5ff0365029d1
gitwangjian <wangjianimustu@163.com>
2016-08-09 17:23:56 +0800

    Update example_test.go
    qwe

--- icmp/example_test.go
+++ icmp/example_test.go
@@ -10,8 +10,8 @@ import (
 	"os"
 	"runtime"
 
-	"golang.org/x/net/icmp"
-	"golang.org/x/net/ipv6"
+	"gitwangjian/net/icmp"
+	"gitwangjian/net/ipv6"
 )
 
 func ExamplePacketConn_nonPrivilegedPing() {
