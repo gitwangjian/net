6842433563b6e8d4166f7af32b083decd0794ad7
gitwangjian <wangjianimustu@163.com>
2016-08-10 09:30:38 +0800

    Update charset_test.go
    a

--- html/charset/charset_test.go
+++ html/charset/charset_test.go
@@ -12,7 +12,7 @@ import (
 	"strings"
 	"testing"
 
-	"gitwangjian/text/transform"
+	"github.com/gitwangjian/text/transform"
 )
 
 func transformString(t transform.Transformer, s string) (string, error) {
