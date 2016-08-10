12c6779d2d4322f483013e2e4064fecc3ee32e55
gitwangjian <wangjianimustu@163.com>
2016-08-10 10:15:20 +0800

    Update control_rfc2292_unix.go
    c

--- ipv6/control_rfc2292_unix.go
+++ ipv6/control_rfc2292_unix.go
@@ -10,7 +10,7 @@ import (
 	"syscall"
 	"unsafe"
 
-	"gitwangjian/net/internal/iana"
+	"github.com/gitwangjian/net/internal/iana"
 )
 
 func marshal2292HopLimit(b []byte, cm *ControlMessage) []byte {
