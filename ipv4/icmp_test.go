ef274353512a045ca115d0dce6b806ec32ad6add
gitwangjian <wangjianimustu@163.com>
2016-08-10 10:03:51 +0800

    Update icmp_test.go
    github.com/

--- ipv4/icmp_test.go
+++ ipv4/icmp_test.go
@@ -10,8 +10,8 @@ import (
 	"runtime"
 	"testing"
 
-	"gitwangjian/net/internal/nettest"
-	"gitwangjian/net/ipv4"
+	"github.com/gitwangjian/net/internal/nettest"
+	"github.com/gitwangjian/net/ipv4"
 )
 
 var icmpStringTests = []struct {
